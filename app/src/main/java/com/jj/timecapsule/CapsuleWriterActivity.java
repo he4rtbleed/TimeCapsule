1533542ecc68790edc05314881dfa62621866d71
seongwonjung <122601269+seongwonjung@users.noreply.github.com>
2024-05-27 14:14:56 +0900

    위치 지정 추가

--- app/src/main/java/com/jj/timecapsule/CapsuleWriterActivity.java
+++ app/src/main/java/com/jj/timecapsule/CapsuleWriterActivity.java
@@ -11,7 +11,6 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -26,31 +25,28 @@ import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 public class CapsuleWriterActivity extends AppCompatActivity implements OnMapReadyCallback {
-    //구글맵 관련 변수
+    // 구글맵 관련 변수
     private GoogleMap map;
     private FusedLocationProviderClient fusedLocationProviderClient;
 
-    //위치권한 관련 변수
+    // 위치권한 관련 변수
     private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
     private static final int DEFAULT_ZOOM = 15;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private boolean locationPermissionGranted;
 
-    //위치 정보 저장 변수
+    // 위치 정보 저장 변수
     private Location lastKnownLocation;
 
     // 사용자가 선택한 위치를 저장할 변수
     private LatLng selectedLocation;
-    //UI 변수
+
+    // UI 변수
     BottomSheetBehavior<LinearLayout> bottomSheetBehavior;
 
-    //https://developers.google.com/maps/documentation/android-sdk/current-place-tutorial?hl=ko
+    // 위치 권한 요청 메서드
     private void getLocationPermission() {
-        /*
-         * Request location permission, so that we can get the location of the
-         * device. The result of the permission request is handled by a callback,
-         * onRequestPermissionsResult.
-         */
+        // 위치 권한 요청. 권한 요청 결과는 onRequestPermissionsResult 콜백으로 처리됨
         if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
                 android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
@@ -67,10 +63,9 @@ public class CapsuleWriterActivity extends AppCompatActivity implements OnMapRea
                                            @NonNull String[] permissions,
                                            @NonNull int[] grantResults) {
         locationPermissionGranted = false;
-        if (requestCode
-                == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {// If request is cancelled, the result arrays are empty.
-            if (grantResults.length > 0
-                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (requestCode == PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION) {
+            // 요청이 취소된 경우 결과 배열은 비어 있음
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 locationPermissionGranted = true;
             }
         } else {
@@ -79,9 +74,7 @@ public class CapsuleWriterActivity extends AppCompatActivity implements OnMapRea
         updateLocationUI();
     }
 
-    // 위치 권한 요청 코드 끝
-
-    // 맵 준비된경우 이벤트핸들러
+    // 맵 준비된 경우 이벤트 핸들러
     @Override
     public void onMapReady(GoogleMap map) {
         this.map = map;
@@ -92,7 +85,7 @@ public class CapsuleWriterActivity extends AppCompatActivity implements OnMapRea
         // "현재 위치" 버튼 활성화
         updateLocationUI();
 
-        // Get the current location of the device and set the position of the map.
+        // 장치의 현재 위치를 파악하고 지도의 위치를 설정
         getDeviceLocation();
 
         // 지도 클릭하여 위치 저장
@@ -101,18 +94,19 @@ public class CapsuleWriterActivity extends AppCompatActivity implements OnMapRea
             public void onMapClick(LatLng latLng) {
                 selectedLocation = latLng; // 사용자가 선택한 위치를 저장
                 map.clear(); // 기존 마커 제거
-                map.addMarker(new MarkerOptions().position(latLng).title("Selected Location")); // 새로운 마커 추가
+                map.addMarker(new MarkerOptions().position(latLng).title("저장 위치")); // 새로운 마커 추가
                 map.moveCamera(CameraUpdateFactory.newLatLng(latLng)); // 카메라를 클릭한 위치로 이동
 
-                // 작동하는지 확인
-                Toast.makeText(CapsuleWriterActivity.this, "Location Selected: " + latLng.latitude + ", " + latLng.longitude, Toast.LENGTH_SHORT).show();
-                Log.d("CapsuleWriterActivity", "Location Selected: " + latLng.latitude + ", " + latLng.longitude);
-
+                // 저장 되었는지 확인
+                if (selectedLocation != null) {
+                    Toast.makeText(CapsuleWriterActivity.this, "선택한 위치: " + selectedLocation.latitude + ", " + selectedLocation.longitude, Toast.LENGTH_SHORT).show();
+                    Log.d("CapsuleWriterActivity", "선택한 위치: " + selectedLocation.latitude + ", " + selectedLocation.longitude);
+                }
             }
         });
     }
 
-    // 위치 업데이트 메서드
+    // 위치 UI 업데이트 메서드
     private void updateLocationUI() {
         if (map == null) {
             return;
@@ -129,28 +123,28 @@ public class CapsuleWriterActivity extends AppCompatActivity implements OnMapRea
                 lastKnownLocation = null;
                 getLocationPermission();
             }
-        } catch (SecurityException e)  { }
+        } catch (SecurityException e) {
+            // 위치 권한 관련 예외 처리
+        }
     }
 
+    // 장치의 현재 위치 가져오기
     private void getDeviceLocation() {
-        /*
-         * 최적 위치 구하기
-         */
         try {
             if (locationPermissionGranted) {
                 Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
                 locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
                     @Override
                     public void onComplete(@NonNull Task<Location> task) {
-                        if (task.isSuccessful()) {
-                            // Set the map's camera position to the current location of the device.
+                        if (task.isSuccessful() && task.getResult() != null) {
+                            // 요청이 성공하고 위치 정보가 있는 경우
                             lastKnownLocation = task.getResult();
-                            if (lastKnownLocation != null) {
-                                map.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                                        new LatLng(lastKnownLocation.getLatitude(),
-                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
-                            }
+                            // 지도의 카메라를 현재 위치로 이동
+                            map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                    new LatLng(lastKnownLocation.getLatitude(),
+                                            lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
                         } else {
+                            // 위치 정보가 없는 경우 기본 위치로 이동
                             map.moveCamera(CameraUpdateFactory
                                     .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
                             map.getUiSettings().setMyLocationButtonEnabled(false);
@@ -158,9 +152,10 @@ public class CapsuleWriterActivity extends AppCompatActivity implements OnMapRea
                     }
                 });
             }
-        } catch (SecurityException e)  { }
+        } catch (SecurityException e) {
+            // 위치 권한 관련 예외 처리
+        }
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -176,13 +171,5 @@ public class CapsuleWriterActivity extends AppCompatActivity implements OnMapRea
 
         // BottomSheetBehavior 설정
         bottomSheetBehavior = BottomSheetBehavior.from(findViewById(R.id.bottom_sheet));
-
-//        bottomSheet.setOnClickListener(v -> {
-//            if (bottomSheetBehavior.getState() == BottomSheetBehavior.STATE_EXPANDED) {
-//                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
-//            } else {
-//                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-//            }
-//        });
     }
 }
